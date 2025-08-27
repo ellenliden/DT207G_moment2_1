9c081f675ce81cc0d15805e888d18d74dfe03292
Ellen <lidenellen@gmail.com>
2025-08-27 15:57:00 +0200

    Lägg till grundläggande Express.js-server

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
-const port = 3000; 
+app.get('/', (req, res)=> {
+res.send ('Hello from my web server');
+});
+app.listen(port, () => {
+console.log(`Server is running on port ${port}`);
+});
 
