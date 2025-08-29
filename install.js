db9b818187baa22111dc0ff11911b19574171537
Ellen <lidenellen@gmail.com>
2025-08-29 15:52:41 +0200

    skapar databasstruktur.

---
+++ install.js
@@ -0,0 +1,39 @@
+const { Client } = require("pg");
+require("dotenv").config();
+
+//Anslut till databasen
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, //måste man ha detta för att kunna ansluta till databasen vid gratis hosting
+  },
+});
+client.connect((err) => {
+  if (err) {
+    console.log("Fel vid anslutning till databasen");
+  } else {
+    console.log("Ansluten till databasen");
+  }
+});
+
+//Skapa tabell
+client.query(`
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        companyname VARCHAR(100) NOT NULL,
+        jobtitle VARCHAR(100) NOT NULL,
+        location VARCHAR(100) NOT NULL,
+        startdate DATE NOT NULL,
+        enddate DATE NULL,
+        is_current BOOLEAN DEFAULT FALSE, //detta visar om användaren fortfarande jobbar på företaget
+        description TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+`);
+
+client.end();
