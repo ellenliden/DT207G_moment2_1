b7d1981fab9cf5e84a992542e124b7ded1624743
Ellen <lidenellen@gmail.com>
2025-08-29 13:57:13 +0200

    Lagt till EJS som template engine, konfigurerat statiska filer skapat en views/-mapp med index.ejs.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=> {
-res.send ('Hello from my web server');
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 app.listen(port, () => {
-console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
